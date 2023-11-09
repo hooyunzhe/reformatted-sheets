47ff1f5f7f8a7eb3f28bab63d0e8bca781c553de
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-11-09 17:13:14 +0800

    chore: Made output_handler follow flake8 conventions

--- srcs/output_handler.py
+++ srcs/output_handler.py
@@ -1,72 +1,97 @@
 import os
 import json
 import pandas as pd
-
 from .exceptions import OutputConfigError
 
+
 class OutputHandler():
-	"""Format data and output sheets based on config"""
+    """
+    Format data and output sheets based on config
 
-	def __init__(self, config_filename: str) -> None:
-		"""Save the config filename"""
+    Methods:
+        read_config_file() -> None:
+            read and parse the config file
+    """
 
-		self.config_filename = config_filename
+    def __init__(self, config_filename: str) -> None:
+        """
+        Save the config filename
 
+        Arguments:
+            config_filename (str):
+                name of the config file
+        """
 
-	def read_config_file(self) -> None:
-		"""Read and parse the config file
+        self.config_filename = config_filename
 
-		Raises:
-			OutputConfigError:
-				"ConfigFileNotFound": the file cannot be found
-				"InvalidSyntax": the syntax is invalid
-				"MissingOutputFileInfo": the output file section is empty
-				"MissingSheetInfo": the sheet section is empty
-				"MissingColumnInfo": the column section is empty
-				"MissingKey": the required keys are missing
-		"""
+    def read_config_file(self) -> None:
+        """
+        Read and parse the config file
 
-		# make sure config file exists
-		if not os.path.isfile("configs/" + self.config_filename):
-			raise OutputConfigError("ConfigFileNotFound", self.config_filename)
+        Raises:
+            OutputConfigError:
+                "ConfigFileNotFound": the file cannot be found
+                "InvalidSyntax": the syntax is invalid
+                "MissingOutputFileInfo": the output file section is empty
+                "MissingSheetInfo": the sheet section is empty
+                "MissingColumnInfo": the column section is empty
+                "MissingKey": the required keys are missing
+        """
 
-		# read and parse the config
-		with open("configs/" + self.config_filename) as config_file:
-			try:
-				self.config = json.load(config_file)
-			except ValueError as error:
-				raise OutputConfigError("InvalidSyntax", self.config_filename, str(error))
+        # make sure config file exists
+        if not os.path.isfile("configs/" + self.config_filename):
+            raise OutputConfigError("ConfigFileNotFound", self.config_filename)
 
-		# handle single output file in config
-		if type(self.config) == dict:
-			self.config = [self.config]
+        # read and parse the config
+        with open("configs/" + self.config_filename) as config_file:
+            try:
+                self.config = json.load(config_file)
+            except ValueError as error:
+                raise OutputConfigError("InvalidSyntax",
+                                        self.config_filename, str(error))
 
-		# make sure the config isn't empty
-		if not self.config:
-			raise OutputConfigError("MissingOutputFileInfo", self.config_filename)
+        # handle single output file in config
+        if type(self.config) is dict:
+            self.config = [self.config]
 
-		# make sure required keys exist
-		missing_keys = []
-		for output_file in self.config:
-			missing_keys.extend([key for key in ["filename", "sheets"] if key not in output_file])
-			if "sheets" in output_file:
-				# handle single sheet in output file
-				sheets = [output_file["sheets"]] if type(output_file["sheets"]) == dict else output_file["sheets"]
+        # make sure the config isn't empty
+        if not self.config:
+            raise OutputConfigError("MissingOutputFileInfo",
+                                    self.config_filename)
 
-				# make sure the sheets aren't empty
-				if not sheets:
-					raise OutputConfigError("MissingSheetInfo", self.config_filename)
+        # make sure required keys exist
+        missing_keys = []
+        for output_file in self.config:
+            missing_keys.extend([key for key in ["filename", "sheets"]
+                                 if key not in output_file])
+            if "sheets" in output_file:
+                # handle single sheet in output file
+                sheets = ([output_file["sheets"]]
+                          if type(output_file["sheets"]) is dict
+                          else output_file["sheets"])
 
-				# make sure required keys exist in sheet
-				for sheet in sheets:
-					missing_keys.extend([key for key in ["name", "title", "type", "columns"] if key not in sheet])
-					if "columns" in sheet:
-						# make sure the columns aren't empty
-						if not sheet["columns"] or type(sheet["columns"]) == dict:
-							raise OutputConfigError("MissingColumnInfo", self.config_filename)
-						missing_keys.extend([key for key in ["from", "name", "type"] for col in sheet["columns"] if key not in col])
+                # make sure the sheets aren't empty
+                if not sheets:
+                    raise OutputConfigError("MissingSheetInfo",
+                                            self.config_filename)
 
-		# raise exception if there are keys missing
-		if missing_keys:
-			raise OutputConfigError("MissingKey", self.config_filename, str(missing_keys))
+                # make sure required keys exist in sheet
+                for sheet in sheets:
+                    missing_keys.extend([key for key in
+                                         ["name", "title", "type", "columns"]
+                                         if key not in sheet])
+                    if "columns" in sheet:
+                        # make sure the columns aren't empty
+                        if (not sheet["columns"]
+                           or type(sheet["columns"]) is dict):
+                            raise OutputConfigError("MissingColumnInfo",
+                                                    self.config_filename)
+                        missing_keys.extend([key for key in
+                                             ["from", "name", "type"]
+                                             for col in sheet["columns"]
+                                             if key not in col])
 
+        # raise exception if there are keys missing
+        if missing_keys:
+            raise OutputConfigError("MissingKey",
+                                    self.config_filename, str(missing_keys))
