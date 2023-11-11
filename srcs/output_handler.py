8fba2c887ba9bf5620a13fe3eadd6685994b26cd
Hoo Yun Zhe <yunzhescientist@gmail.com>
2023-11-11 16:26:35 +0800

    feat: Implemented basic output file generation in output_handler

--- srcs/output_handler.py
+++ srcs/output_handler.py
@@ -96,21 +96,66 @@ class OutputHandler():
             raise OutputConfigError("MissingKey",
                                     self.config_filename, str(missing_keys))
 
-    def generate_output_files(self, data: pd.DataFrame) -> None:
+    def generate_output_path(self, filename: str) -> str:
+        """
+        Generate full path of the output file based on arguments
+
+        Arguments:
+            filename (str):
+                name of the output file
+
+        Returns:
+            full path of the output file
+        """
+
+        return f'output_files/{filename}.xlsx'
+
+    def format_output_sheet(self,
+                            data_df: pd.DataFrame,
+                            sheet: any) -> pd.DataFrame:
+        """
+        Format data into a dataframe for a single sheet based on config
+
+        Arguments:
+            data_df (pd.DataFrame):
+                dataframe containing data to format
+            sheet (object):
+                config object of the sheet
+
+        Returns:
+            formatted dataframe for a single sheet
+        """
+
+        sheet_df = pd.DataFrame()
+
+        for column in sheet["columns"]:
+            sheet_df[column["name"]] = data_df[column["from"]]
+
+        return sheet_df
+
+    def generate_output_files(self, data_df: pd.DataFrame) -> None:
         """
         Format data based on config and generate output files
 
         Arguments:
-            data (pd.DataFrame):
+            data_df (pd.DataFrame):
                 dataframe containing data to generate output from
         """
 
-        print(data)
+        print(data_df)
+
+        if not os.path.isdir("output_files"):
+            os.mkdir("output_files")
 
         # format data and generate output for each output file
         for output_file in self.config:
+            # get path and open file for writing
+            output_path = self.generate_output_path(output_file["filename"])
+            with pd.ExcelWriter(output_path) as writer:
+                # format and save data in a dataframe for each sheet
+                for sheet in output_file["sheets"]:
 
-            # format and save data in a dataframe for each sheet
-            for sheet in output_file["sheets"]:
-                with pd.ExcelWriter("output_files/test.xlsx") as writer:
-                    data.to_excel(writer, sheet_name="test_sheet")
+                    sheet_df = self.format_output_sheet(data_df, sheet)
+                    sheet_df.to_excel(writer,
+                                      sheet_name=sheet["name"],
+                                      index=False)
